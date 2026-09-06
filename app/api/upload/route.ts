610386e08cf667f19602ac5ff88b1fab424445fc
Sreejamanthena <sreejamanthena44@gmail.com>
2026-09-06 12:56:54 +0530

    Enhance comments for file upload handling
    Added comments to clarify the upload process and validation steps.

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -3,13 +3,16 @@ import cloudinary from '@/lib/cloudinary';
 
 export async function POST(req: Request) {
   try {
-    // Get form data
+    // the req is an obj conatinf the method,api,body etcc.. from that we acquire the body using the req.formdata()
+    // Get form data 
     const formData = await req.formData();
 
     // Get uploaded file
+    // this contains the file obj such as the filename type ,size binary data etc....
     const file = formData.get('file') as File;
 
-    // Validation
+    // Validation 
+    // here if no file is uploaded then the file obj is null that indicates false 
     if (!file) {
       return NextResponse.json(
         { error: 'No file uploaded' },
@@ -17,12 +20,13 @@ export async function POST(req: Request) {
       );
     }
 
-    // Convert file to buffer
+    // Convert file to buffer here we extract the binary data to the browser buffer 
     const bytes = await file.arrayBuffer();
-
+    // clloudinary reqiures either stream or buffer so we will convert the binary data to buffer using the buffer.from() method
     const buffer = Buffer.from(bytes);
 
     // Upload to Cloudinary
+    // creats an strem to store the data to the cloudinary 
     const result: any = await new Promise(
       (resolve, reject) => {
         cloudinary.uploader
@@ -32,15 +36,18 @@ export async function POST(req: Request) {
               folder: 'certvault',
               type: 'private', // Secure storage
             },
+            //callback executed by the clodinary after the upload is completed it has two 
+            //parameters error and result if any error occured it will be stored in the error parameter 
+            // and if the upload is successful then the result will be stored in the result parameter containg the secure url and the pub id and the other data
             (error, result) => {
               if (error) {
-                reject(error);
+                reject(error); // if any error occured than passes the flow to the catch block 
               } else {
-                resolve(result);
+                resolve(result); // sucess stores the response in the resullt 
               }
             }
           )
-          .end(buffer);
+          .end(buffer); // this passes the data through the stream 
       }
     );
 
