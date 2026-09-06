70e91198d423d516bbfad07d2701e0d4b3864277
Sreejamanthena <sreejamanthena44@gmail.com>
2026-09-06 13:01:01 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -10,8 +10,8 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
+// initialize the connection to the database for the project once and use it through out the project
 const app = initializeApp(firebaseConfig);
-
+// export the auth and db objects to be used in the project for authentication and database operations 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
